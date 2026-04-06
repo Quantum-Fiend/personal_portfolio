62ee5ec4eacf46e60c831a30cee974d89093f9fd
Troff_Junkie <tusharbisht706@gmail.com>
2026-04-06 20:43:01 +0530

    Fix

---
+++ src/Components/home/hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+	<div>
+	  this is my hero section
+	</div>
+  )
+}
+
+export default Hero
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import React from "react";
 
-const page = () => {
-  return <div>this is my main page</div>;
+const main = () => {
+  return (
+    <div>
+      <h1>This is my personal </h1>
+    </div>
+  );
 };
 
-export default page;
+export default main;
